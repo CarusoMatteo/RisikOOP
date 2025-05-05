01472eb0c0b30fae4f0abab4d5912fe6b48e526c
Francesco Sacripante <francesco.sacripante@studio.unibo.it>
2025-05-05 18:19:42 +0200

    Modifying the view interface

--- src/main/java/it/unibo/risikoop/view/Interfaces/RisikoView.java
+++ src/main/java/it/unibo/risikoop/view/Interfaces/RisikoView.java
@@ -4,8 +4,15 @@ import it.unibo.risikoop.controller.RisikoApp;
 
 public interface RisikoView {
 
-    void setObserver(RisikoApp risikoApp);
+    public void start();
 
-    void start();
+    /** */
+    public void chose_map();
+
+    /** */
+    public void begin_play();
+
+    /** */
+    public void game_over();
 
 }
