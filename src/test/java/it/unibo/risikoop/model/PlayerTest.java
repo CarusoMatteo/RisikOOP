ffeafbe249ffa5976ae05ec6d765cf38614f5f78
Matteo Caruso <matteo.caruso7@studio.unibo.it>
2025-07-08 12:24:59 +0200

    Changed indentation from tabs to spaces to fix Checkstyle error

--- src/test/java/it/unibo/risikoop/model/PlayerTest.java
+++ src/test/java/it/unibo/risikoop/model/PlayerTest.java
@@ -16,28 +16,28 @@ import it.unibo.risikoop.model.interfaces.Player;
  * Class to test adding Players.
  */
 public class PlayerTest {
-        private final GameManager gameManager = new GameManagerImpl();
-        private final String armandoName = "Armando";
-        private final String diegoName = "Diego";
-        private final String bobName = "bob";
-        private final String claraName = "Clara";
+    private final GameManager gameManager = new GameManagerImpl();
+    private final String armandoName = "Armando";
+    private final String diegoName = "Diego";
+    private final String bobName = "bob";
+    private final String claraName = "Clara";
 
-        @Test
-        void addPlayer() {
-                final List<Player> players = List.of(new PlayerImpl(armandoName, new Color(0, 0, 0)),
-                                new PlayerImpl(diegoName, new Color(0, 2, 0)));
-                players.forEach(i -> gameManager.addPlayer(i.getName(), i.getColor()));
-                assertEquals(players, gameManager.getPlayers());
-                gameManager.addPlayer(bobName, new Color(1, 0, 0));
-                assertEquals(List.of(new PlayerImpl(armandoName, new Color(0, 0, 0)),
-                                new PlayerImpl(diegoName, new Color(0, 2, 0)),
-                                new PlayerImpl(bobName, new Color(1, 0, 0))), gameManager.getPlayers());
-                assert (gameManager.removePlayer(bobName));
-                assertNotEquals(List.of(new PlayerImpl(armandoName, new Color(0, 0, 0)),
-                                new PlayerImpl(diegoName, new Color(0, 2, 0)),
-                                new PlayerImpl(bobName, new Color(1, 0, 0))), gameManager.getPlayers());
-                assert (!gameManager.removePlayer(claraName));
-                assertEquals(List.of(new PlayerImpl(armandoName, new Color(0, 0, 0)),
-                                new PlayerImpl(diegoName, new Color(0, 2, 0))), gameManager.getPlayers());
-        }
+    @Test
+    void addPlayer() {
+        final List<Player> players = List.of(new PlayerImpl(armandoName, new Color(0, 0, 0)),
+                new PlayerImpl(diegoName, new Color(0, 2, 0)));
+        players.forEach(i -> gameManager.addPlayer(i.getName(), i.getColor()));
+        assertEquals(players, gameManager.getPlayers());
+        gameManager.addPlayer(bobName, new Color(1, 0, 0));
+        assertEquals(List.of(new PlayerImpl(armandoName, new Color(0, 0, 0)),
+                new PlayerImpl(diegoName, new Color(0, 2, 0)),
+                new PlayerImpl(bobName, new Color(1, 0, 0))), gameManager.getPlayers());
+        assert (gameManager.removePlayer(bobName));
+        assertNotEquals(List.of(new PlayerImpl(armandoName, new Color(0, 0, 0)),
+                new PlayerImpl(diegoName, new Color(0, 2, 0)),
+                new PlayerImpl(bobName, new Color(1, 0, 0))), gameManager.getPlayers());
+        assert (!gameManager.removePlayer(claraName));
+        assertEquals(List.of(new PlayerImpl(armandoName, new Color(0, 0, 0)),
+                new PlayerImpl(diegoName, new Color(0, 2, 0))), gameManager.getPlayers());
+    }
 }
