0fb2c9f31090b774ebbee43d641d73dac5e0b968
Matteo Caruso <matteo.caruso7@studio.unibo.it>
2025-07-12 13:25:29 +0200

    Implemented testWildAllEqual and testAllDifferent tests

---
+++ src/test/java/it/unibo/risikoop/model/cards/ComboCheckerTest.java
@@ -0,0 +1,12 @@
+package it.unibo.risikoop.model.cards;
+
+import it.unibo.risikoop.model.implementations.gamecards.combos.ComboCheckerImpl;
+import it.unibo.risikoop.model.interfaces.cards.ComboChecker;
+
+/**
+ * Class to test Combo Checkers.
+ */
+final class ComboCheckerTest {
+    private final ComboChecker checker = new ComboCheckerImpl();
+
+}
