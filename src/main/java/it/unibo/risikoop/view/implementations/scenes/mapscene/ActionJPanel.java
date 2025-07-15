8bb44389e9f82a198005867ad075e0e4794f6d48
Matteo Ceccarelli <matteo.ceccarelli12@studio.unibo.it>
2025-07-15 11:07:06 +0200

    WIP: safe code before merge on mster branch

--- src/main/java/it/unibo/risikoop/view/implementations/scenes/mapscene/ActionJPanel.java
+++ src/main/java/it/unibo/risikoop/view/implementations/scenes/mapscene/ActionJPanel.java
@@ -5,6 +5,7 @@ import javax.swing.JPanel;
 /**
  * Panel for the Action Buttons in the MapScene.
  */
+// commit before merge
 public final class ActionJPanel extends JPanel {
     private static final long serialVersionUID = 1L;
 }
