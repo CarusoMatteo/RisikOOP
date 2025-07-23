7362d0f15daa2493688a4d399a2b3e16bc54e854
Francesco Sacripante <francesco.sacripante@studio.unibo.it>
2025-07-23 15:20:01 +0200

    small adjustment to the impelmentastion of the controller

--- src/main/java/it/unibo/risikoop/controller/implementations/ControllerImpl.java
+++ src/main/java/it/unibo/risikoop/controller/implementations/ControllerImpl.java
@@ -85,7 +85,7 @@ public final class ControllerImpl implements Controller {
         });
 
     }
-
+    
     @Override
     public GamePhaseController getGamePhaseController() {
         return gamePhaseController;
