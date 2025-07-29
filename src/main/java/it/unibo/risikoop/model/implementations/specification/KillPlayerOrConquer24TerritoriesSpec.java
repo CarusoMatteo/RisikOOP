5b5e6edf7bba27743e421a10f168457064af8438
Matteo Ceccarelli <matteo.ceccarelli12@studio.unibo.it>
2025-07-29 12:49:59 +0200

    fix style error

--- src/main/java/it/unibo/risikoop/model/implementations/specification/KillPlayerOrConquer24TerritoriesSpec.java
+++ src/main/java/it/unibo/risikoop/model/implementations/specification/KillPlayerOrConquer24TerritoriesSpec.java
@@ -7,7 +7,8 @@ import it.unibo.risikoop.model.interfaces.Player;
 import it.unibo.risikoop.model.interfaces.Specification;
 
 /**
- * Specification that checks if a player has either killed a specified target player
+ * Specification that checks if a player has either killed a specified target
+ * player
  * or conquered at least 24 territories.
  * This specification combines two conditions using logical OR.
  */
@@ -17,7 +18,8 @@ public final class KillPlayerOrConquer24TerritoriesSpec implements Specification
     private final int requiredTerritories;
 
     /**
-     * Constructs a KillPlayerOrConquer24TerritoriesSpec with the specified target player.
+     * Constructs a KillPlayerOrConquer24TerritoriesSpec with the specified target
+     * player.
      * The required number of territories is set to the default value of 24.
      *
      * @param target the player that must
@@ -30,9 +32,11 @@ public final class KillPlayerOrConquer24TerritoriesSpec implements Specification
     @Override
     public boolean isSatisfiedBy(final PlayerGameContext candidate) {
         final Specification<PlayerGameContext> killPlayer = new KillPlayerSpec(target);
-        // final Specification<PlayerGameContext> conquerTerritories = new ConquerTerritoriesSpec(requiredTerritories);
-        final Specification<PlayerGameContext> conquerTerritories = new ConquerTerritoriesWithMinArmiesSpec(requiredTerritories, 1);
-
+        // final Specification<PlayerGameContext> conquerTerritories = new
+        // ConquerTerritoriesSpec(requiredTerritories);
+        final Specification<PlayerGameContext> conquerTerritories = new ConquerTerritoriesWithMinArmiesSpec(
+            requiredTerritories,
+            1);
 
         return killPlayer.or(conquerTerritories).isSatisfiedBy(candidate);
     }
